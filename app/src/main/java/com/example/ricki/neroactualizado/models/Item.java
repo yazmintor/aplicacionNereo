542379247c037addd6246aaebf5bb800cfad372e
ricki2099 <ricki2099@hotmail.com>
2016-08-24 15:39:00 -0500

    Update Item.java

--- app/src/main/java/com/example/ricki/neroactualizado/models/Item.java
+++ app/src/main/java/com/example/ricki/neroactualizado/models/Item.java
@@ -33,7 +33,7 @@ public class Item implements Serializable{
     public String getNombre() {
         return nombre;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
@@ -41,79 +41,79 @@ public class Item implements Serializable{
     public String getAcum() {
         return acum;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setAcum(String acum) { this.acum = acum; }
 
     public String getDefini() {
         return defini;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setDefini(String defini) {
         this.defini = defini;
     }
 
 
     public String getPorce1() { return porce1; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorce1(String porce1) { this.porce1 = porce1; }
 
     public String getNota1() { return nota1; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNota1(String nota1) { this.nota1 = nota1; }
 
     public String getPorce2() { return porce2; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorce2(String porce2) { this.porce2 = porce2;  }
-
+// se hace la adaptacion que envie la  informacion
     public String getNota2() { return nota2; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNota2(String nota2) { this.nota2 = nota2; }
 
     public String getPorce3() { return porce3; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorce3(String porce3) { this.porce3 = porce3; }
 
     public String getNota3() { return nota3; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNota3(String nota3) { this.nota3 = nota3; }
 
     public String getPorce4() { return porce4; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorce4(String porce4) { this.porce4 = porce4; }
 
     public String getNota4() { return nota4; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNota4(String nota4) { this.nota4 = nota4; }
 
     public String getPorce5() { return porce5; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorce5(String porce5) { this.porce5 = porce5; }
 
     public String getNota5() { return nota5; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNota5(String nota5) { this.nota5 = nota5; }
 
     public String getPorce6() { return porce6; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorce6(String porce6) { this.porce6 = porce6; }
 
     public String getNota6() { return nota6; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNota6(String nota6) { this.nota6 = nota6; }
 
     public String getPorcelab() { return porcelab; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorcelab(String porcelab) { this.porcelab = porcelab; }
 
     public String getNotalab() { return notalab; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNotalab(String notalab) { this.notalab = notalab; }
 
     public String getPorceexa() { return porceexa; }
-
+// se hace la adaptacion que envie la  informacion
     public void setPorceexa(String porceexa) { this.porceexa = porceexa; }
 
     public String getNotaexa() { return notaexa; }
-
+// se hace la adaptacion que envie la  informacion
     public void setNotaexa(String notaexa) { this.notaexa = notaexa; }
 }
