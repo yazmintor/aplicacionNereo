d87b96bc232e18fa61992371e1ce23fa1aceba53
ricki2099 <ricki2099@hotmail.com>
2016-08-24 15:36:18 -0500

    Update Dia.java

--- app/src/main/java/com/example/ricki/neroactualizado/models/Dia.java
+++ app/src/main/java/com/example/ricki/neroactualizado/models/Dia.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
  */
 public class Dia implements Serializable {
 
+// se declaran las variables a utilizar
     private String nombredia;
 
     private String asigcod;
@@ -22,6 +23,7 @@ public class Dia implements Serializable {
     private String docente;
     private String apellido;
 
+// se declaran los get y set de cada una de las variables
     public Dia(){}
     public Dia(String nombredia) {
         this.nombredia = nombredia;
@@ -31,16 +33,16 @@ public class Dia implements Serializable {
     public String getNombredia() {
         return nombredia;
     }
-
+// se hace la adaptacion que envie la  informacion 
     public void setNombredia(String nombredia) {
         this.nombredia = nombredia;
     }
-
+// se hace la adaptacion que envie la  informacion
 
     public String getAsigcod() {
         return asigcod;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setAsigcod(String asigcod) {
         this.asigcod = asigcod;
     }
@@ -48,7 +50,7 @@ public class Dia implements Serializable {
     public String getNomasig() {
         return nomasig;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setNomasig(String nomasig) {
         this.nomasig = nomasig;
     }
@@ -56,7 +58,7 @@ public class Dia implements Serializable {
     public String getCodigocra() {
         return codigocra;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setCodigocra(String codigocra) {
         this.codigocra = codigocra;
     }
@@ -64,7 +66,7 @@ public class Dia implements Serializable {
     public String getGrupocod() {
         return grupocod;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setGrupocod(String grupocod) {
         this.grupocod = grupocod;
     }
@@ -72,7 +74,7 @@ public class Dia implements Serializable {
     public String getDianombre() {
         return dianombre;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setDianombre(String dianombre) {
         this.dianombre = dianombre;
     }
@@ -80,7 +82,7 @@ public class Dia implements Serializable {
     public String getHora() {
         return hora;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setHora(String hora) {
         this.hora = hora;
     }
@@ -100,11 +102,11 @@ public class Dia implements Serializable {
     public void setSedenombre(String sedenombre) {
         this.sedenombre = sedenombre;
     }
-
+// se hace la adaptacion que envie la  informacion
     public String getEdinombre() {
         return edinombre;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setEdinombre(String edinombre) {
         this.edinombre = edinombre;
     }
@@ -112,7 +114,7 @@ public class Dia implements Serializable {
     public String getSalnombre() {
         return salnombre;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setSalnombre(String salnombre) {
         this.salnombre = salnombre;
     }
@@ -120,7 +122,7 @@ public class Dia implements Serializable {
     public String getDocente() {
         return docente;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setDocente(String docente) {
         this.docente = docente;
     }
@@ -128,7 +130,7 @@ public class Dia implements Serializable {
     public String getApellido() {
         return apellido;
     }
-
+// se hace la adaptacion que envie la  informacion
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
